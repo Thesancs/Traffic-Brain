0b43a1304051ae1e6013ce9e2f597ac3784e6321
Glauber Santos <Santos.Glauber2002@gmail.com>
2025-10-17 12:09:34 +0000

    Agora então varie os valores quero ver se muda a animação

--- src/app/dashboard/meta/data.ts
+++ src/app/dashboard/meta/data.ts
@@ -81,11 +81,11 @@ export type ConversionFunnelStage = {
 
 // Data based on the reference image
 export const conversionFunnelData: ConversionFunnelStage[] = [
-  { stage: 'Cliques', value: 15 },
-  { stage: 'Vis. Página', value: 10 },
-  { stage: 'ICs', value: 1 },
-  { stage: 'Vendas Inic.', value: 12 },
-  { stage: 'Vendas Apr.', value: 4 },
+  { stage: 'Cliques', value: 20 },
+  { stage: 'Vis. Página', value: 15 },
+  { stage: 'ICs', value: 8 },
+  { stage: 'Vendas Inic.', value: 5 },
+  { stage: 'Vendas Apr.', value: 2 },
 ];
 
     
